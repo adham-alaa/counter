674ab5d1540826b0e4e4028d13428a0316c097b7
adham-alaa <adhamalaa8086@gmail.com>
2025-04-04 00:50:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ let count = 0;
 document.getElementById("decrement").onclick = function () {
     count = count - 1;
     document.getElementById("counter").innerHTML = count;
+    if (count === 69) {
+        alert("Nice!");
+    }
 };
 
 document.getElementById("reset").onclick = function () {
     count = 0;
     document.getElementById("counter").innerHTML = count;
+    
 };
 
 document.getElementById("increment").onclick = function () {
     count = count + 1;
     document.getElementById("counter").innerHTML = count;
+    if (count === 69) {
+        alert("Nice!");
+    }
 };
+
